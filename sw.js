38e4573b6cd3255e9c77318a452b989643fa9ee6
hm5829 <hm5829@yahoo.com.tw>
2018-04-30 23:29:37 +0800

    ad

--- sw.js
+++ sw.js
@@ -1,9 +1,26 @@
+const cacheVersion = 'v1';
+const filesToCache = [
+  'index.html',
+  'register_sw.js',
+  
+];
 self.addEventListener('install', event => {
   console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(cacheVersion)
+    .then(cache => {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
+  console.log('[ServiceWorker] Activate');
 });
 self.addEventListener('fetch', event => {
   console.log('[ServiceWorker] fetch', event.request);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => response || fetch(event.request))
+  );
 });
