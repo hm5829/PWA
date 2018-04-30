e6c662acf21e6e9413bea80b586666d41458d250
hm5829 <hm5829@yahoo.com.tw>
2018-04-30 23:00:52 +0800

    a

--- register_sw.js
+++ register_sw.js
@@ -1,10 +1,12 @@
+
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
-    .then(reg => {
-      // registration worked
-      console.log('[Service Worker] Registration succeeded. Scope is ' + reg.scope);
-    }).catch(error => {
-      // registration failed
-      console.log('[Service Worker] Registration failed with ' + error);
-    });
+window.addEventListener('load', function() {
+navigator.serviceWorker.register('/sw.js').then(function(registration) {
+// Registration was successful
+console.log('ServiceWorker registration successful with scope: ', registration.scope);
+}).catch(function(err) {
+// registration failed :(
+console.log('ServiceWorker registration failed: ', err);
+});
+});
 }
